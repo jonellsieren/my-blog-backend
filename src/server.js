741935875bce8febf44da816a94a03a414c4be95
jonellsieren <70969803+jonellsieren@users.noreply.github.com>
2021-06-24 11:33:07 -0500

    Rewrote the upvote endpoint
    Added upvotes for article

--- src/server.js
+++ src/server.js
@@ -9,29 +9,47 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/api/articles/:name', async (req, res) => {
-
     try {
         const articleName = req.params.name;
 
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('my-blog');
     
-        const articlesInfo = await db.collection('articles').findOne({ name: articleName })
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
         res.status(200).json(articleInfo);
     
         client.close();
-
     } catch (error) {
-        res.status(500).json({ message: 'Error connecting to db', error});
+        res.status(500).json({ message: 'Error connecting to db', error });
     }
-
+    
 })
 
-app.post('/api/articles/:name/upvote', (req, res) => {
+app.post('/api/articles/:name/upvote', async (req, res) => {
+
+    try {
+
     const articleName = req.params.name;
 
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!!`);
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    const db = client.db('my-blog');
+
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    await db.collection('articles').updateOne({ name: articleName}, {
+        '$set' : {
+            upvotes: articleInfo.upvotes + 1,
+        },
+    });
+
+    const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+
+    client.close();
+} catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+}
+
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
@@ -41,6 +59,7 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
     articlesInfo[articleName].comments.push({ username, text });
 
     res.status(200).send(articlesInfo[articleName]);
+
 });
 
 
